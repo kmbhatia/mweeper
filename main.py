43466bec00d519cf5276a8d4e6d63fbfd3c7f8f7
Karan <karanbhatia@protonmail.com>
2022-05-04 12:32:26 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import settings
+import utils
+from cell import Cell
+
+root = Tk()
+
+# Writing Window Settings
+
+root.configure(bg="black")
+
+# root.geometry(WidthxHeight)
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+
+root.title("Mweeper")
+
+#root.resizable(Width, Height)
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='black',
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75),
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25),
+)
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c= Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+
+# Run the Window
+root = mainloop()
