6cd3524df6b5de4f64e68afb692856923d04dac9
Karan <karanbhatia@protonmail.com>
2022-05-07 09:41:18 +0530

    Game Development Completed

--- cell.py
+++ cell.py
@@ -1,45 +1,75 @@
-from tkinter import Button
+from tkinter import *
 import settings
 import random
+from tkinter import messagebox #as messagebox
+import sys
+
 class Cell:
 
-    all=[]
+    all = []
+    cell_count_label_object = None
+    cell_count = settings.CELL_COUNT
 
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
-        self.x= x
-        self.y= y
+        self.x = x
+        self.y = y
+        self.is_opened = False
+        self.is_mine_candidate = False
 
-        #append the object to the cell.all list
+        # append the object to the cell.all list
         Cell.all.append(self)
 
     def create_btn_object(self, location):
         btn = Button(
             location,
             width=12,
-            height=4, 
+            height=4,
         )
 
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
-    
+
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg="black",
+            fg="white",
+            text=f"Cells Left:{Cell.cell_count}",
+            width=12,
+            height=4,
+            font=("", 24)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
             self.show_cell()
-    
+            if self.surrounded_cells_mines_length == 0:
+                for cell_object in self.surrounded_cells:
+                    cell_object.show_cell()
+
+            if Cell.cell_count == settings.MINES_COUNT:
+                messagebox.showinfo("Game Over!", "Congratulations, You have won the game!")
+                sys.exit()
+
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+
     def get_cell_by_axis(self, x, y):
-        #Return a cell object based on the value of x and y
+        # Return a cell object based on the value of x and y
         for cell in Cell.all:
-            if cell.x==x and cell.y==y:
+            if cell.x == x and cell.y == y:
                 return cell
-    
+
     @property
     def surrounded_cells(self):
-        cells= [
+        cells = [
             self.get_cell_by_axis(self.x-1, self.y-1),
             self.get_cell_by_axis(self.x-1, self.y),
             self.get_cell_by_axis(self.x-1, self.y+1),
@@ -51,26 +81,40 @@ class Cell:
         ]
         cells = [cell for cell in cells if cell is not None]
         return cells
-    
+
     @property
     def surrounded_cells_mines_length(self):
         counter = 0
         for cell in self.surrounded_cells:
             if cell.is_mine:
-                counter+=1
+                counter += 1
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(
+                text=self.surrounded_cells_mines_length)
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}")
+                self.is_opened = True
+        self.cell_btn_object.configure(bg='#d9d9d9')
 
     def show_mine(self):
-        #a logic to interrup the game and display a message that the player lost!
         self.cell_btn_object.configure(bg='red')
-    
+        #ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine!', 'Game Over!', 0)
+        messagebox.showinfo("Game Over!", "You have clicked on a mine!")
+        sys.exit()
+
     def right_click_actions(self, event):
-        print(event)
-        print("I am right clicked")
-    
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(bg='orange')
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(bg='#d9d9d9')
+            self.is_mine_candidate=False
+
     @staticmethod
     def randomize_mines():
         picked_cells = random.sample(
@@ -82,4 +126,3 @@ class Cell:
 
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
-        
--- main.py
+++ main.py
@@ -24,7 +24,16 @@ top_frame = Frame(
     height=utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
-
+game_title = Label(
+    top_frame,
+    bg = 'black',
+    fg = 'white',
+    text = 'Mweeper!',
+    font=('', 45)
+)
+game_title.place(
+    x=utils.width_prct(35),
+)
 left_frame = Frame(
     root,
     bg='black',
@@ -51,8 +60,9 @@ for x in range(settings.GRID_SIZE):
         c.cell_btn_object.grid(
             column=x, row=y
         )
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(x=0, y=0)
 Cell.randomize_mines()
-for c in Cell.all:
-    print(c.is_mine)
+
 # Run the Window
 root = mainloop()
